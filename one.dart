4dabb0a2e82c0d10ecc430443498638177be2da9
yashgoyall <yash.12014858@lpu.in>
2023-01-17 04:25:14 +0530

    Data type conversion

--- one.dart
+++ one.dart
@@ -1,1 +1,18 @@
-//fiirts try
+void main() {
+
+  //data type conversion 
+  
+  print("good morning");
+  int Myno = 8;
+  String MynoStr = Myno.toString();
+  print(MynoStr);
+  String mobno = "50";
+  String mobNoStr = "50.5";
+  int monboInt = int.parse(mobno);
+  print(monboInt);
+  double mobNoDouble = double.parse(mobNoStr);
+  print(mobNoDouble);
+  for (int j = 0; j < 10; j++) {
+    print(j);
+  }
+}
